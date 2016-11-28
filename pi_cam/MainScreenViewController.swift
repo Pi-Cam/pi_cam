c69ecff3f37a6837952d66538f7914dc57eee9c0
Marquavious Draggon <marq.draggon@gmail.com>
2016-11-27 17:08:25 -0800

    segues and what not

---
+++ pi_cam/MainScreenViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainScreenViewController.swift
+//  pi_cam
+//
+//  Created by Marquavious on 11/27/16.
+//  Copyright © 2016 marqmakesapps. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "streamingServicerCell") as! StreamingServiceTableViewCell
+        
+        return cell
+    }
+
+}
---
+++ pi_cam/StreamingServiceTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  StreamingServiceTableViewCell.swift
+//  pi_cam
+//
+//  Created by Marquavious on 11/27/16.
+//  Copyright © 2016 marqmakesapps. All rights reserved.
+//
+
+import UIKit
+
+class StreamingServiceTableViewCell: UITableViewCell {
+    @IBOutlet weak var streamingServiceImage: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
