bbe224922a7f19bf31aa34e887048c753cdc8345
Marquavious Draggon <marq.draggon@gmail.com>
2016-11-29 16:17:29 -0800

    pivot time

---
+++ pi_cam/MainScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InitialScreenVC.swift
+//  pi_cam
+//
+//  Created by Marquavious on 11/27/16.
+//  Copyright © 2016 marqmakesapps. All rights reserved.
+//
+
+import UIKit
+
+class InitialScreenVC: UIViewController{
+    
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
