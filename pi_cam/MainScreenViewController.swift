b6000703fe8d89852f89208f55f7a3b88d11dd67
Marquavious Draggon <marq.draggon@gmail.com>
2016-11-29 18:44:03 -0800

    animations

--- pi_cam/MainScreenViewController.swift
+++ pi_cam/MainScreenViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class InitialScreenVC: UIViewController{
-    
+class InitialScreenVC: UIViewController {
+
     // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(piImage)
+        setUpPiImage()
+        
+        UIView.animate(withDuration: 0.7, delay: 0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.piImage.alpha = 1.0
+        }, completion: nil)
+    
+        UIView.animate(withDuration: 1, delay: 1, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.piImage.center.y -= 60
+        }, completion: nil)
     }
     
+    lazy var piImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.alpha = 0.0
+        imageView.image = UIImage(named: "pi")
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    func setUpPiImage(){
+        piImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        piImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        piImage.frame = CGRect(x:(view.bounds.size.width/2) - 67.5, y: (view.bounds.size.height/2) - 86, width: 135, height: 172)
+    }
 }
