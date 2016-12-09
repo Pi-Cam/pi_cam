6b93ee2871f666c10d0cef9e90b55e32da58cc1e
Marquavious Draggon <marq.draggon@gmail.com>
2016-12-08 18:15:14 -0800

    animation for the win

--- pi_cam/SelectWifiVC.swift
+++ pi_cam/SelectWifiVC.swift
@@ -17,25 +17,77 @@ class SelectWifiVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var callToActionLabel: UILabel!
     @IBOutlet weak var piImage: UIImageView!
+//    @IBOutlet weak var WifiName: UITextField!
+    
+    @IBOutlet weak var wifiName: UITextField!
+    @IBOutlet weak var wifiPassword: UITextField!
     
     //MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        view.addSubview(wifiTextField)
+        view.addSubview(passwordTextField)
+        setUpWifiTextField()
+        setUpPasswordTextField()
+//        print(textField.translatesAutoresizingMaskIntoConstraints = false)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.autoresizingMask = UIViewAutoresizing.flexibleHeight
+        
+       
     
-        //This is for the bluetooth connection
-        nrfManager = NRFManager(
-        onConnect: {print("C: ★ Connected")},
-        onDisconnect: {print("C: ★ Disconnected")},
-        onData: {(data:Data?, string:String?)->() in print("C: ⬇ Received data - String: \(string) - Data: \(data)")},
-        autoConnect: false)
-            nrfManager.verbose = true
-            nrfManager.delegate = self
+//        //This is for the bluetooth connection
+//        nrfManager = NRFManager(
+//        onConnect: {print("C: ★ Connected")},
+//        onDisconnect: {print("C: ★ Disconnected")},
+//        onData: {(data:Data?, string:String?)->() in print("C: ⬇ Received data - String: \(string) - Data: \(data)")},
+//        autoConnect: true)
+//            nrfManager.verbose = true
+//            nrfManager.delegate = self
+//        
         }
- 
+    
+    let wifiTextField: UITextField = {
+       var textField = UITextField()
+//        textField.backgroundColor = .red
+        textField.font = UIFont(name: "Avenir Next", size: 19)
+        textField.textAlignment = .center
+//        textField.style
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = "Makeschool"
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        var textField = UITextField()
+        //        textField.backgroundColor = .red
+        textField.font = UIFont(name: "Avenir Next", size: 19)
+        textField.textAlignment = .center
+        //        textField.style
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = "Password"
+        return textField
+    }()
+    
+    func setUpWifiTextField(){
+        NSLayoutConstraint(item: wifiTextField, attribute: .leading, relatedBy: .equal, toItem: tableView, attribute: .trailing, multiplier: 1, constant: view.frame.size.width+2).isActive = true
+        wifiTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        wifiTextField.backgroundColor = .gray
+        wifiTextField.widthAnchor.constraint(equalToConstant: (view.frame.size.width/1.5)).isActive = true
+        wifiTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    func setUpPasswordTextField(){
+        NSLayoutConstraint(item: passwordTextField, attribute: .leading, relatedBy: .equal, toItem: tableView, attribute: .trailing, multiplier: 1, constant: view.frame.size.width+2).isActive = true
+        passwordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        passwordTextField.backgroundColor = .gray
+        passwordTextField.widthAnchor.constraint(equalToConstant: (view.frame.size.width/1.5)).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    
+
     //MARK: TableView Functions
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.isHidden = true
@@ -53,7 +105,8 @@ class SelectWifiVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     //MARK: Bluetooth Functions
     func sendData() {
-        let string = "Wifi,MakeSchool,applynow"
+//        let string = "Wifi,MakeSchool,applynow"
+        let string = "wifi,\(wifiName.text!),\(wifiPassword.text!)"
         let result = self.nrfManager.writeString(string)
         print("⬆ Sent string: \(string) - Result: \(result)")
     }
@@ -69,7 +122,23 @@ class SelectWifiVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     }
     
     @IBAction func sendData(_ sender: Any) {
-        sendData()
+
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+//            self.wifiTextField.center.x -= 200
+            self.wifiTextField.center.x = self.view.center.x
+            self.wifiTextField.center.y -= 30
+            
+//            self.passwordTextField.center.x -= 200
+            self.passwordTextField.center.x = self.view.center.x
+            self.passwordTextField.center.y += 30
+            
+
+            self.tableView.alpha = 0
+        }, completion: nil)
+        
+        
+//        sendData()
+//        NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
     }
     
     //MARK: Directional Buttons
